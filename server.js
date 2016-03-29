818780c3d7dd5060d90c233d6c0fb1f3a9f808ce
JamesWilson <wesstation2@gmail.com>
2016-03-29 17:28:02 +0800

    新增使用者資訊 首頁模板建立 View Engine

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-var express  =  require('express'),
-	mongoose =  require('mongoose'),
-	ejs      = 	require('ejs'),
-	ejsMate  =  require('ejs-mate'),
-	morgan   =  require('morgan');
-	
+var express    =  require('express'),
+	mongoose   =  require('mongoose'),
+	ejs        =  require('ejs'),
+	ejsMate    =  require('ejs-mate'),
+	morgan     =  require('morgan'),
+	bodyParser =  require('body-parser');
+
+//var User       =  require('./models/user');
+
+
+var app = express();
+
 //	TODO:Create a WEB API engine
-	
-	
+	app.use(morgan('dev'));
+	app.use(bodyParser.json());
+	app.engine('ejs',ejsMate);
+	app.set('view engine','ejs');
 	//TODO:reset Web app Route,Create a template
 	app.get('/' , function(req,res){
-		res.render();
+		res.render('home');
 	})
 	
 	
@@ -17,7 +25,7 @@ var express  =  require('express'),
 //	WARAING:Test Port
 	app.listen(3000,function(err){
 		if(err) throw err;
-		
+		console.log('Hello 3000');
 	})
 	
 	
